e64e79a81aa14e5dcab3b697fb3a2a3b45b42ddb
StevenSu <suwei00@gmail.com>
2019-07-26 14:02:11 +0800

    add comments to second s3 bucket

--- Lab01/app.py
+++ Lab01/app.py
@@ -7,9 +7,9 @@ from lab01.lab01_stack import Lab01Stack
 
 app = core.App()
 
-#create 2 bucket different region
 Lab01Stack(app, "lab01-ap",env={"region":"ap-northeast-1"})
-Lab01Stack(app, "lab01-us",env={"region":"us-west-2"})
+#create second bucket different region
+#Lab01Stack(app, "lab01-us",env={"region":"us-west-2"})
 
 
 app.synth()
