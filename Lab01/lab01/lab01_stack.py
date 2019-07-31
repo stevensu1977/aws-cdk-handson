c0f3c6196d60407d3bcafd0d2f71b114b85e885e
StevenSu <suwei00@gmail.com>
2019-07-31 20:16:31 +0800

    update lab01/lab01_stack.py

--- Lab01/lab01/lab01_stack.py
+++ Lab01/lab01/lab01_stack.py
@@ -3,8 +3,9 @@ from aws_cdk import (
     aws_s3 as s3
 )
 
-class Lab01Stack(core.Stack):
 
+class Lab01Stack(core.Stack):
+    
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
